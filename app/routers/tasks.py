9dcf2431e2edbe383f8c4fe5f0951f51dadc1105
Alexander Thomas <alex.b.thomas@icloud.com>
2026-07-03 02:12:54 -0500

    adding more operations, adding return type, cleanup

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -1,17 +1,25 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from app.schemas.task import Task, TaskCreate
 from app.services import task_service
 
 router = APIRouter()
 
 @router.get("/")
-def get_tasks():
+def get_tasks() -> list[Task]:
     return task_service.get_tasks()
 
 @router.get("/{task_id}")
-def get_task(task_id: int):
+def get_task(task_id: int) -> Task:
     return task_service.get_task(task_id)
 
 @router.post("/")
-def create_task(task: TaskCreate):
+def create_task(task: TaskCreate) -> Task:
     return task_service.create_task(task)
+
+@router.put("/{task_id}")
+def update_task(task_id: int, task: TaskCreate) -> Task:
+    return task_service.update_task(task_id, task)
+
+@router.delete("/{task_id}", status_code=204)
+def delete_task(task_id: int) -> None:
+    task_service.delete_task(task_id)
--- app/services/task_service.py
+++ app/services/task_service.py
@@ -3,20 +3,29 @@ from app.schemas.task import Task, TaskCreate
 
 tasks: list[Task] = []
 
-def get_tasks():
+def get_tasks() -> list[Task]:
     return tasks
 
-def get_task(task_id: int):
+def get_task(task_id: int) -> Task:
     for task in tasks:
         if task.id == task_id:
             return task
 
     raise HTTPException(status_code=404, detail="Task not found")
 
-def create_task(task: TaskCreate):
+def create_task(task: TaskCreate) -> Task:
     new_id = len(tasks) + 1
     new_task = Task(id=new_id, title=task.title)
     tasks.append(new_task)
 
     return new_task
 
+def update_task(task_id: int, task: TaskCreate) -> Task:
+    found_task = get_task(task_id)
+    found_task.title = task.title
+    
+    return found_task
+
+def delete_task(task_id: int) -> None:
+    found_task = get_task(task_id)
+    tasks.remove(found_task)
